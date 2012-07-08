dc396d987518822432ac6b4d5d1de61ae65e4e67
José Tomás Albornoz <jojo@eljojo.net>
2012-07-08 19:19:59 -0400

    ahora si que si mostrando stats

--- client.js
+++ client.js
@@ -1,15 +1,13 @@
-socket_server = (window.location.hostname.indexOf('heroku') > 0) ? 'http://eljojo.net:3456' : 'http://'+window.location.hostname
+//socket_server = (window.location.hostname.indexOf('heroku') > 0) ? 'http://eljojo.net:3456' : 'http://'+window.location.hostname
+socket_server = 'http://eljojo.net:3456'
 var socket = io.connect(socket_server);
 $(function() {
   var chart = new SmoothieChart();
   var clicks = new TimeSeries();
   chart.addTimeSeries(clicks, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
   chart.streamTo(document.getElementById("clicks_stats"), 600);
-  
   socket.emit('quieroMisStatsConQuesoAHORA')
   socket.on('stats!', function(stats) {
-    console.log(stats)
-    clicks.append(stats.date, stats.clicks);
+    clicks.append(new Date(stats.date).getTime(), stats.clicks);
   })
-  
 })
